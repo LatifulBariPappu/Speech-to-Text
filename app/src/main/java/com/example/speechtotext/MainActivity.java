5def03462e83efd5d44a852b31285e9882ba7e1c
LatifulBari <latifulbari2001@gmail.com>
2023-06-26 18:55:08 +0600

    completed

--- app/src/main/java/com/example/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/speechtotext/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.speechtotext;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
+    TextView textTv;
+    ImageButton voiceBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textTv=findViewById(R.id.textTv);
+        voiceBtn=findViewById(R.id.voiceBtn);
+
+        voiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speak();
+            }
+        });
+    }
+
+    private void speak() {
+        //Intent to show speech to text dialog
+        Intent intent =new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Hi Speak Something...");
+
+        //start intent
+        try{
+            //in there was no error
+            startActivityForResult(intent,REQUEST_CODE_SPEECH_INPUT);
+        }catch (Exception e){
+            //if there was some error
+            //get message of error and show
+            Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    //recieve voice input and handle it
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case REQUEST_CODE_SPEECH_INPUT:{
+                if(requestCode==RESULT_OK && null!=data){
+                    //get text array from voice intent
+                    ArrayList<String> result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    //set to textView
+                    textTv.setText(result.get(0));
+                }
+                break;
+            }
+        }
     }
 }
